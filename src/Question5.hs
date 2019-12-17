c910f5d23028a05cb5ead38123880f64fc8ae5e7
Sam Kerr <sam.p.kerr@gmail.com>
2019-12-17 03:49:08 +0000

    finish 5

--- src/Question5.hs
+++ src/Question5.hs
@@ -17,26 +17,26 @@ pairIndexAndInstructions vals = helper vals 0
         helper [] i = []
         helper (v:vs) i = (i,v) : helper vs (i+1)
 
--- state of the program
--- The stack is unneccsary but thought it might be useful for later questions
+boolToInt :: Bool -> Int
+boolToInt True  = 1
+boolToInt False = 0
+
 type States = HM.HashMap Int Int  
 data PartialFunction = RecieveArg (Arg -> ExecutionState) 
 type Arg = (ExecutionState, Int)
 data ExecutionState = ExecutionState 
   { state  :: States   
-  , stack  :: [PartialFunction]  
+  , pc     :: Int 
   }   
 
 setState :: HM.HashMap Int Int -> ExecutionState -> ExecutionState
 setState hm ex = ex {state = hm}
 
-pushStack :: PartialFunction -> ExecutionState -> ExecutionState
-pushStack f ex = ex {stack = (f:(stack ex))}
+increasePC :: Int -> ExecutionState -> ExecutionState
+increasePC inc ex = ex {pc = (pc ex) + inc}
 
-popStack :: ExecutionState -> (PartialFunction,ExecutionState) 
-popStack ex = (top,newEx)
-    where top = (head . stack) ex 
-          newEx = ex {stack = (tail . stack) ex}
+setPC :: Int -> ExecutionState -> ExecutionState
+setPC newPC ex = ex {pc = newPC}
 
 type Opcode = Int 
 type Instruction = Int 
@@ -49,15 +49,20 @@ getOperandValue (opVal, Position) currStates = fromMaybe (trace "FAILED TO FIND 
 getImmediate (opVal, _) = opVal
 getPosition (opVal, _) currStates = fromMaybe (trace "FAILED TO FIND VALUE" 0) $ HM.lookup opVal currStates
 
--- Handles + and * instructions
+-- Handles 
+    -- maths +,*
+    -- contitions < , ==
 execute :: (Opcode, [Operand]) -> ExecutionState -> IO ExecutionState
 execute (op1,[op2,op3,op4]) executionState = do 
     let currStates = state executionState   
         [val2,val3] = map (\operand -> getOperandValue operand currStates) [op2,op3]
         val4Imm = getImmediate op4 --op4 is always an immediate of an address
+        setValueAtVal4 val = return $ setState (HM.insert val4Imm val currStates) executionState 
     case op1 of
-        1 -> return $ setState (HM.insert val4Imm (val2 + val3) currStates) executionState  
-        2 -> return $ setState (HM.insert val4Imm (val2 * val3) currStates) executionState
+        1 -> setValueAtVal4 (val2 + val3)   
+        2 -> setValueAtVal4 (val2 * val3) 
+        7 -> setValueAtVal4 (boolToInt $ val2 < val3)
+        8 -> setValueAtVal4 (boolToInt $ val2 == val3)
 
 -- Handles IO instructions
 execute (op1,[op2]) executionState = do
@@ -71,8 +76,15 @@ execute (op1,[op2]) executionState = do
             (putStrLn . show) val2
             return executionState
 
--- handles conditions 
-
+-- handles jumps 
+execute (op1,[op2,op3]) executionState = do 
+    let val3 = getOperandValue op3 $ state executionState
+        val2 = getOperandValue op2 $ state executionState
+    case op1 of 
+        5 -> if val2 /= 0 then return $ setPC val3 executionState
+             else return executionState
+        6 -> if val2 == 0 then return $ setPC val3 executionState
+             else return executionState
 
 getOpcodeAndModes :: Instruction -> (Opcode, [Mode])
 getOpcodeAndModes instruction = (read opcode, parameterModes)
@@ -84,33 +96,43 @@ getOpcodeAndModes instruction = (read opcode, parameterModes)
             | c == '1' = Immediate
             | otherwise = trace "Could not decode operand mode" Position
 
-decode :: Instruction -> Int -> ExecutionState -> ((Opcode, [Operand]), Int)
-decode i pc executionState = case getOpcodeAndModes i of 
-    (1,modes) -> ((1, toOps 3 modes), pc+4)
-    (2,modes) -> ((2, toOps 3 modes), pc+4)
-    (3,modes) -> ((3, toOps 1 modes), pc+2)
-    (4,modes) -> ((4, toOps 1 modes), pc+2)
-    _ -> trace "Operand not recognised" $ ((4, toOps 1 []), pc+2)
+decode :: Instruction -> ExecutionState -> ((Opcode, [Operand]), Int)
+decode i executionState = case getOpcodeAndModes i of 
+    (1,modes) -> ((1, toOps 3 modes), 4) 
+    (2,modes) -> ((2, toOps 3 modes), 4) 
+    (3,modes) -> ((3, toOps 1 modes), 2) 
+    (4,modes) -> ((4, toOps 1 modes), 2)
+    (5,modes) -> ((5, toOps 2 modes), 3)
+    (6,modes) -> ((6, toOps 2 modes), 3)
+    (7,modes) -> ((7, toOps 3 modes), 4)
+    (8,modes) -> ((8, toOps 3 modes), 4)
+    _ -> trace "Operand not recognised" $ ((4, toOps 1 []), 2)
     where toOps operandCount modes = zip (getNextK operandCount) modes
-          getNextK k = mapMaybe (\key -> HM.lookup key (state executionState)) [pc+1..pc+k]
+          getNextK k = mapMaybe (\key -> HM.lookup key (state executionState)) [currPc+1..currPc+k]
+          currPc = pc executionState
 
-run :: Int -> Int -> ExecutionState -> IO ExecutionState
-run 99 _ executionState = return executionState
-run instruction pc executionState = do 
-    let (ops,newPc) = decode instruction pc executionState
+-- Halt if instruction is 99
+decodeAndExecute :: Int -> ExecutionState -> IO ExecutionState
+decodeAndExecute 99 executionState = return executionState
+decodeAndExecute instruction executionState = do 
+    let (ops,pcIncriment) = decode instruction executionState
     updatedStates <- execute ops executionState
-    runFrom newPc updatedStates
+    let jumpNotPerfomed = pc updatedStates == pc executionState
+        updatedPCAndStates =  if jumpNotPerfomed then increasePC pcIncriment updatedStates
+                              else updatedStates
+    runUntilHalt updatedPCAndStates
 
-runFrom :: Int -> ExecutionState -> IO ExecutionState
-runFrom pc executionState = do 
-    let instr = fromMaybe (trace "PC did not point to instruction in state" 0) $ HM.lookup pc (state executionState)
-    run instr pc executionState
+fetch :: ExecutionState -> Int 
+fetch executionState = fromMaybe (trace "PC did not point to instruction in state" 0) $ HM.lookup (pc executionState) (state executionState)
 
+runUntilHalt :: ExecutionState -> IO ExecutionState
+runUntilHalt executionState = decodeAndExecute (fetch executionState) executionState
+    
 solve1 :: [Int] -> IO ()
 solve1 instructions = do 
-    runFrom 0 initialEx 
+    runUntilHalt initialEx 
     return ()
     where indexInstructionPairs = pairIndexAndInstructions instructions
-          initialEx = ExecutionState {state = HM.fromList indexInstructionPairs, stack =[]} 
+          initialEx = ExecutionState {state = HM.fromList indexInstructionPairs, pc = 0} 
     
     
